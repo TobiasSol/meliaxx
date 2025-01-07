4637cb6383b6da4eec32a93286b09adabd366a88
Dein Name <deine.email@example.com>
2025-01-07 05:11:45 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,52 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: [
-      'wacbmitvfgopmwvtdkvz.supabase.co' // Add Supabase storage domain
+      'wacbmitvfgopmwvtdkvz.supabase.co',
+      'supabasekong-mo4k4og0sgck8k0kg4kk08w4.145.223.103.240.sslip.io',
+      'meliaxx.de'
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'wacbmitvfgopmwvtdkvz.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'supabasekong-mo4k4og0sgck8k0kg4kk08w4.145.223.103.240.sslip.io',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'meliaxx.de',
+        pathname: '/**',
+      }
     ],
   },
-}
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-eval' 'unsafe-inline' https://js.stripe.com;
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' data: blob: https: http:;
+              font-src 'self' data:;
+              connect-src 'self' 
+                https://api.stripe.com 
+                https://wacbmitvfgopmwvtdkvz.supabase.co 
+                http://supabasekong-mo4k4og0sgck8k0kg4kk08w4.145.223.103.240.sslip.io;
+              frame-src 'self' https://js.stripe.com;
+            `.replace(/\s{2,}/g, ' ').trim()
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;

4ab2d34dfc6c6530c7d40b9f096b892cac80d1ef
Dein Name <deine.email@example.com>
2025-01-13 21:21:48 +0100

    update links banner

--- components/OnlyFansBanner.js
+++ components/OnlyFansBanner.js
@@ -289,7 +289,7 @@ const OnlyFansBanner = ({ onVisibilityChange }) => {
             {/* Chat Button mit Glanz-Animation */}
             <div className="absolute left-[47%] bottom-20 w-[260px]">
               <a 
-                href="https://onlyfans.com/meliax"
+                href="https://onlyfans.com/meliax/c19"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="relative overflow-hidden bg-transparent border-2 border-[#e3cbaa] text-[#e3cbaa] hover:text-black font-bold py-3 px-6 rounded-full transition-all duration-300 transform hover:scale-105 shadow-lg text-base group inline-flex items-center justify-center"
